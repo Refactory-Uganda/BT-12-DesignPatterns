b5fbb20bc0689813075f0ff4270ba0c4f721fb74
alindaByamukama <alindabyamukama@gmail.com>
2023-03-01 10:26:23 +0300

    add changes to decorator ts file - instantiation added

--- Decorator.ts
+++ Decorator.ts
@@ -130,5 +130,20 @@ class Whip extends CondimentDecorator {
     }
 }
 
+// ORDER COFFEE
 
+// plain beverage no additions
+let beverage: Beverage = new Espresso();
+console.log(`${beverage.getDescription()} $${beverage.cost()}`);
 
+// decorated beverage
+let beverage2: Beverage = new DarkRoast();
+beverage2 = new Mocha(beverage2);
+beverage2 = new Whip(beverage2);
+console.log(`${beverage2.getDescription()} $${beverage2.cost()}`);
+
+let beverage3: Beverage = new HouseBlend();
+beverage3 = new Soy(beverage3);
+beverage3 = new Mocha(beverage3);
+beverage3 = new Whip(beverage3);
+console.log(`${beverage3.getDescription()} $${beverage3.cost()}`);
